cfdbcc792f616c05cab4110256e65753b9e44c9f
MMR <moj2raj2@gmail.com>
2023-07-08 21:31:17 +0330

    edit panel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,5 +32,6 @@ class AppServiceProvider extends ServiceProvider
 //            Variable::MODELS[Business::class] => Business::class,
 
         ]);
+        Schema::defaultStringLength(191);
     }
 }
