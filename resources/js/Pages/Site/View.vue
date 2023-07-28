81035a744b857fc318fdf3ae366526e9e3ff8ed6
MMR <moj2raj2@gmail.com>
2023-07-28 14:23:55 +0330

    added auto site view

--- resources/js/Pages/Site/View.vue
+++ resources/js/Pages/Site/View.vue
@@ -196,9 +196,10 @@ export default {
       let seconds = this.$page.props.reward_second;
       // seconds = 5;
       const intervalId = setInterval(function () {
-        this.timer++;
-        this.timerPercent = Math.floor((this.timer / seconds) * 100)
-
+        if (!document.hidden) {
+          this.timer++;
+          this.timerPercent = Math.floor((this.timer / seconds) * 100)
+        }
         if (this.timer >= seconds) {
           clearInterval(intervalId);
           this.addTransaction();
@@ -244,7 +245,44 @@ export default {
             this.isLoading(false);
           });
     }
-  }
+  },
+  tabIsVisible() {
+    var hidden = "hidden";
+
+    // Standards:
+    if (hidden in document)
+      document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+      document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+      document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+      document.addEventListener("msvisibilitychange", onchange);
+    // IE 9 and lower:
+    else if ("onfocusin" in document)
+      document.onfocusin = document.onfocusout = onchange;
+    // All others:
+    else
+      window.onpageshow = window.onpagehide
+          = window.onfocus = window.onblur = onchange;
+
+    function onchange(evt) {
+      var v = "visible", h = "hidden",
+          evtMap = {
+            focus: v, focusin: v, pageshow: v, blur: h, focusout: h, pagehide: h
+          };
+
+      evt = evt || window.event;
+      if (evt.type in evtMap)
+        document.body.className = evtMap[evt.type];
+      else
+        document.body.className = this[hidden] ? "hidden" : "visible";
+    }
+
+    // set the initial state (but only if browser supports the Page Visibility API)
+    if (document[hidden] !== undefined)
+      onchange({type: document[hidden] ? "blur" : "focus"});
+  },
 }
 
 </script>
